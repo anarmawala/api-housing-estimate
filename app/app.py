a87609b66b1b9bb07afd32faf1ee84d03e09c297
Arshad Narmawala <arshadn00@gmail.com>
2020-10-25 10:33:07 -0500

    Fix

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 # coding: utf-8
 
@@ -210,6 +210,7 @@ def priceComparison(zipcode, radius, cur_beds, cur_baths, current_payment):
 
 
 @app.route("/", methods=["POST"])
+@cross_origin()
 def hello():
     req_data = request.get_json()
     city = str(req_data["city"])
